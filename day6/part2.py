670fcf414d5f55eefc2a9b8702b77303b59c6f5d
SpawnTerror <sp4wnt3rr0r@gmail.com>
2022-12-25 20:11:04 +0000

    Day 6 Completed

--- day6/part2.py
+++ day6/part2.py
@@ -3,50 +3,53 @@
 # AOC Day 6 Part 2
 
 # Working with 14 letters in a row
+
 setSize = 14
 
 # Grab each letter and put into a list
+
 def getData(fileName: str) -> list:
     with open(fileName, 'r') as f:
         initialData = [x for x in f.readline()]
     return initialData
 
 # Part 2
+
 def processData(data: list):
 
     # Create dictionary to count occurences
     # Marker is already on 14 as first set is 14 long
+
     dictionarySet = {}
     markerPosition = 14
 
     # Itterate through every letter 
     # (-3) to end on a full set of 14
     # Put 14 letters into a line
+
     for x in range(0, len(data)-3):
         dictionarySet = {}
         line = data[x: x + setSize]
 
         # Count occurences of each letter
         # in the current dictionary of 14
+
         for letter in line:
             if letter in dictionarySet:
                 dictionarySet[letter] += 1
             else:
                 dictionarySet[letter] =1
 
-        # Check if all counts are equal to 1
-        # If true, return the set and marker position
+        # As letters are added as keys
+        # and duplicates are merged
+        # Stop when we count to 14
+        # which means all letters are unique
         
         if len(dictionarySet) == 14:
             return dictionarySet, markerPosition
         else:
             markerPosition += 1
-        '''
-        if all(value == 1 for value in dictionarySet.values()):
-            return dictionarySet, markerPosition
-        else:
-            markerPosition += 1
-        '''
+        
 data = getData('day6/input.txt')
 uniqueSet, markerPosition = processData(data)
 
---
+++ day7/part1.py
@@ -0,0 +1,3 @@
+# SpawnTerror 2022
+# Python 3.11.1
+# AOC Day 7 Part 1
