7f5e91324345fbb431edbcb435e74fad1f665ca4
SpawnTerror <sp4wnt3rr0r@gmail.com>
2022-12-28 17:44:35 +0000

    Update part1.py

--- day11/part1.py
+++ day11/part1.py
@@ -50,3 +50,14 @@ for x in monkeyDict:
 
 # Test calculations
 print(f'New value = ', calculateStress(79, 1))
+
+''' Example output
+Monkey 0 =  {'Items': [93, 98], 'Stress': '* 17', 'Divide': 19, 'True': 5, 'False': 3}
+Monkey 1 =  {'Items': [95, 72, 98, 82, 86], 'Stress': '+ 5', 'Divide': 13, 'True': 7, 'False': 6}
+Monkey 2 =  {'Items': [85, 62, 82, 86, 70, 65, 83, 76], 'Stress': '+ 8', 'Divide': 5, 'True': 3, 'False': 0}        
+Monkey 3 =  {'Items': [86, 70, 71, 56], 'Stress': '+ 1', 'Divide': 7, 'True': 4, 'False': 5}
+Monkey 4 =  {'Items': [77, 71, 86, 52, 81, 67], 'Stress': '+ 4', 'Divide': 17, 'True': 1, 'False': 6}
+Monkey 5 =  {'Items': [89, 87, 60, 78, 54, 77, 98], 'Stress': '* 7', 'Divide': 2, 'True': 1, 'False': 4}
+Monkey 6 =  {'Items': [69, 65, 63], 'Stress': '+ 6', 'Divide': 3, 'True': 7, 'False': 2}
+Monkey 7 =  {'Items': [89], 'Stress': '* old', 'Divide': 11, 'True': 0, 'False': 2}
+'''
